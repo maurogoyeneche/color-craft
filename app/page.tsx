8164d4d5e5cec15bf673009c36178230d9f18271
Mauro Goyeneche <maurogoyeneche@gmail.com>
2025-01-25 21:48:48 -0300

    gap

--- app/page.tsx
+++ app/page.tsx
@@ -15,7 +15,7 @@ export default function Home() {
        <ContactForm />
       </main>
       
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
+      <footer className="row-start-3 flex flex-wrap items-center justify-center">
       </footer>
     </div>
   )
