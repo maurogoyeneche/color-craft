4e92f6da45fa10fc52c57d0603fd223939dcb1ef
Mauro Goyeneche <maurogoyeneche@gmail.com>
2025-01-25 21:42:45 -0300

    validation

--- lib/schema.ts
+++ lib/schema.ts
@@ -3,7 +3,7 @@ import { z } from "zod";
 export const contactFormSchema = z.object({
   color: z
     .string()
-    .min(2, { message: "Debes agregar al menos un color" })
+    .min(1, { message: "Debes agregar al menos un color" })
     .max(32, { message: "Tu fórmnula de color es muy commpleja" }),
   message: z
     .string()
