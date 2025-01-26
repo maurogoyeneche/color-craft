0554178fb99b82305bbf74869a71e445855bd98e
Mauro Goyeneche <48460577+maurogoyeneche@users.noreply.github.com>
2025-01-25 21:21:12 -0300

    [FTR] Add color form (#2)
    * add basic form
    
    * Add result, and styles

---
+++ lib/schema.ts
@@ -0,0 +1,14 @@
+import { z } from "zod";
+
+export const contactFormSchema = z.object({
+  color: z
+    .string()
+    .min(2, { message: "Debes agregar al menos un color" })
+    .max(32, { message: "Tu fórmnula de color es muy commpleja" }),
+  message: z
+    .string()
+    .min(2, { message: "El comentario debe tener al menos 2 caracteres" })
+    .max(1000, {
+      message: "El comentario debe tener menos de 1000 caracteres",
+    }),
+});
