2ddf34750df36843df38a5824f7bcbbdb55f875f
Mauro Goyeneche <maurogoyeneche@gmail.com>
2025-02-01 13:14:46 -0300

    dependencie

--- components/ui/color-form.tsx
+++ components/ui/color-form.tsx
@@ -12,7 +12,6 @@ import {
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Button } from "@/components/ui/button";
-import { Textarea } from "@/components/ui/textarea";
 import { cn } from "@/lib/utils";
 
 import { contactFormAction } from "@/lib/actions";
